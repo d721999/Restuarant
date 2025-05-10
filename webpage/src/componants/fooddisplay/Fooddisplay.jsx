4a81ee5453d03afee97c025074d49d6773bc484e
deepak <thakur721998@gmail.com>
2025-05-10 20:15:37 +0530

    first commit

--- webpage/src/componants/fooddisplay/Fooddisplay.jsx
+++ webpage/src/componants/fooddisplay/Fooddisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import {StoreContext} from '../../context/StoreContext'
-import Fooditem from '../fooditem/Fooditem'
+import FoodItem from '../fooditem/FoodItem'
 const FoodDisplay = ({category}) => {
     const {food_list} = useContext(StoreContext)
   return (
@@ -11,7 +11,7 @@ const FoodDisplay = ({category}) => {
             {food_list.map((item,index) =>{
               if (category==='all' || category==item.category)
               {
-                return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
               }
             })
            
