b091fbae50da04ead427313c5e1f4536d0bd4068
d721999 <thakur721998@gmail.com>
2025-05-10 16:42:35 +0530

    provide the live backend url

--- webpage/src/context/StoreContext.jsx
+++ webpage/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cardItems, setCardItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://restuarant-gbs3.onrender.com";
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
