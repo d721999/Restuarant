b1dd13a983a053d94b22aa47dda5c643b206d2f2
deepak <thakur721998@gmail.com>
2025-05-10 21:58:02 +0530

    first commit

--- webpage/src/pages/home/Home.jsx
+++ webpage/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React,{ useState } from 'react'
 import './Home.css'
 
 import Exploremenu from '../../componants/exploremenu/exploremenu'
-import FoodDisplay from '../../componants/foodDisplay/foodDisplay';
+import FoodDisplay from "../../components/foodDisplay/foodDisplay";
 import AppDownload from '../../componants/AppDownload/AppDownload'
 import Banner from '../../componants/food banner/Banner'
 import FoodSlider from '../../componants/slider/FoodSlider'
