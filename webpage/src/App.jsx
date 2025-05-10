8dbbf443cd386f511f7a96f392caff45369bd836
deepak <thakur721998@gmail.com>
2025-05-10 19:26:41 +0530

    first commit

--- webpage/src/App.jsx
+++ webpage/src/App.jsx
@@ -1,13 +1,13 @@
 import React,{ useState } from 'react';
-import Navbar from './componants/navbar/Navbar'
-import Home from './pages/home/Home'
-import Cart from './pages/Cart/Cart'
-import Placeorder from './pages/placeorder/Placeorder'
-import { Routes,Route } from 'react-router-dom'
-import Footer from './componants/Footer/Footer'
-import Loginpopup from './componants/Login-popup/Loginpopup'
-import Verify from './pages/Verify/Verify';
-import MyOrders from './pages/myOrder/MyOrders'
+import Navbar from './componants/navbar/Navbar';
+import Home from './pages/home/Home';
+import Cart from './pages/Cart/Cart';
+import Placeorder from './pages/placeorder/Placeorder';
+import { Routes,Route } from 'react-router-dom';
+import Footer from './componants/Footer/Footer';
+import Loginpopup from './componants/Login-popup/Loginpopup';
+import Verify from './pages/verify/Verify';
+import MyOrders from './pages/myOrder/MyOrders';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const App = () => {
