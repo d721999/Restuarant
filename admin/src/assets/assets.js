0e17333733e514a749311fefeccc53f29c61bb79
d721999 <thakur721998@gmail.com>
2025-05-15 16:50:39 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,7 +4,7 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
-export const url ="https://restuarant-gbs3.onrender.com"
+export const url ="https://admin-wmnl.onrender.com"
 export const assets ={
     logo,
     add_icon,
